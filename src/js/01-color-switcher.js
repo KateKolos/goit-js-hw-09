799d5bd4e27ab7ba382ce938b7fad4f695922ab8
Kate Kolosovska-Shchudlo <kate.shchudlo@gmail.com>
2023-02-04 18:33:27 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+function getRandomHexColor() {
+  return ` #${Math.floor(Math.random() * 16777215).toString(16)} `;
+}
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
 
+stopBtn.setAttribute('disabled', '');
+let intervalID = 0;
+
+startBtn.addEventListener('click', onStartBtnClick);
+
+function onStartBtnClick(evt) {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+  intervalID = setInterval(() => {
+    generatedColor = getRandomHexColor();
+    bodyEl.style.backgroundColor = generatedColor;
+  }, 1000);
+}
+
+stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStopBtnClick(evt) {
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+
+  clearInterval(intervalID);
+}
