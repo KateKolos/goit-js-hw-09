159147f5d15d166ca0ed030608fd0d24d2ff8483
Kate Kolosovska-Shchudlo <kate.shchudlo@gmail.com>
2023-02-05 22:22:09 +0200

    add randonHEX js file

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,6 +1,5 @@
-function getRandomHexColor() {
-  return ` #${Math.floor(Math.random() * 16777215).toString(16)} `;
-}
+import getRandomHexColor from './randomHEX';
+
 const startBtn = document.querySelector('button[data-start]');
 const stopBtn = document.querySelector('button[data-stop]');
 const bodyEl = document.querySelector('body');
